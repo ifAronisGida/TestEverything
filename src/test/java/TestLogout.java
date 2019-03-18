4a19109922787dfdf94b0fb0ee79115f60f3cd15
Horinka Peter <horinka.peter@gmail.com>
2019-03-18 13:38:52 +0100

    added logout testcase

---
+++ src/test/java/TestLogout.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupi
+
+public class TestLogout {
+
+    private static Driver driver = new Driver();
+
+    @Test
+    public void testLogout(){}
+
+
+}
