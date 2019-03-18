4d2cadeff0086d7594888471d6e463e1757d034f
Horinka Peter <horinka.peter@gmail.com>
2019-03-18 14:22:25 +0100

    test for logout implemented

--- src/test/java/TestLogout.java
+++ src/test/java/TestLogout.java
@@ -1,13 +1,32 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.junit.jupi
 
-public class TestLogout {
+class TestLogout {
 
-    private static Driver driver = new Driver();
+    private static WebDriver driver = new Driver().getDriver();
+
+    @BeforeAll
+    static void login(){
+        driver.get("https://jira.codecool.codecanvas.hu/secure/Dashboard.jspa");
+        driver.findElement(By.id("login-form-username")).sendKeys("user14");
+        driver.findElement(By.id("login-form-password")).sendKeys("CCPass123");
+        driver.findElement(By.id("login")).click();
+
+    }
 
     @Test
-    public void testLogout(){}
+    void logoutNavigatesToRelogURL() throws InterruptedException {
+        Thread.sleep(5000);
+        String expectedLogoutUrl ="https://jira.codecool.codecanvas.hu/secure/Logout!default.jspa?atl_token=BSF4-29UN-JXKU-E3U4_994844cd7a224e754b63728be9453777c02a6392_lin";
+        driver.findElement(By.id("header-details-user-fullname")).click();
+        driver.findElement(By.id("log_out")).click();
+        Assertions.assertEquals(expectedLogoutUrl, "https://jira.codecool.codecanvas.hu/secure/Logout!default.jspa?atl_token=BSF4-29UN-JXKU-E3U4_994844cd7a224e754b63728be9453777c02a6392_lin");
+        driver.close();
+    }
 
 
 }
