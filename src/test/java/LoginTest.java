c567ef78d926f6c7b4e89f7c5869ea0cfd7fbd72
Aron Gida <ifAronisGida@github.com>
2019-03-18 13:17:50 +0100

    added driver class

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoginTest {
+
+    private static WebDriver driver = new Driver().getDriver();
+
+    @Test
+    void testSuccessfulLogin() throws InterruptedException {
+        driver.get("https://jira.codecool.codecanvas.hu/login.jsp");
+        WebElement userName = driver.findElement(By.id("login-form-username"));
+        userName.sendKeys("user121");
+        WebElement userPassword = driver.findElement(By.id("login-form-password"));
+        userPassword.sendKeys("CCPass123");
+        WebElement loginButton = driver.findElement(By.id("login-form-submit"));
+        loginButton.click();
+        Thread.sleep(1000);
+        String urlToTest = "https://jira.codecool.codecanvas.hu/secure/Dashboard.jspa";
+        String actualUrl = driver.getCurrentUrl();
+        assertEquals(urlToTest, actualUrl);
+        driver.close();
+    }
+}
