c7d9bcc826c992996b221026596ef018c024c496
Áron <arkheonos@gmail.com>
2019-04-02 14:17:04 +0200

    added browse projects pom class

---
+++ src/main/java/JiraBrowseProjects.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class JiraBrowseProjects {
+
+    private WebDriver driver;
+
+    @FindBy(id = "browse_link")
+    private WebElement projectsNavbarButton;
+
+    @FindBy(id = "project_view_all_link_lnk")
+    private WebElement viewAllProjectsButton;
+
+    public JiraBrowseProjects() {
+        driver = new Driver().getDriver();
+        PageFactory.initElements(driver, this);
+    }
+
+    public void navigateToViewAllProjects() {
+        projectsNavbarButton.click();
+        viewAllProjectsButton.click();
+    }
+
+    public int countProjects() {
+        return driver.findElements(By.xpath("//table[@class='aui']/tbody/tr")).size();
+    }
+
+}
