ea873bcfdd0253ad513e20e9dda8dddb98c1cb0d
Horinka Peter <horinka.peter@gmail.com>
2019-05-02 13:43:55 +0200

    refactor: createNewIssue should now click the create button first

--- src/main/java/JiraCreateIssue.java
+++ src/main/java/JiraCreateIssue.java
@@ -74,8 +74,7 @@ public class JiraCreateIssue extends JiraPOM{
     }
 
     private void selectFromDropdown(WebElement element, String select) {
-        wait.until(ExpectedConditions.elementToBeClickable(element));
-
+        waitForElementClickable(element);
         element.sendKeys(select);
 
         if (!element.getAttribute("aria-activedescendant").equals("null")) {
@@ -97,6 +96,7 @@ public class JiraCreateIssue extends JiraPOM{
     }
 
     void createNewIssue(String projectName, String issueName, String summary) {
+        clickCreateButton();
         selectFromDropdown(projectField, projectName);
         selectFromDropdown(issueTypeField, issueName);
         fillInSummaryField(summary);
